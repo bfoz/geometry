a7b251f0c5e2f69e60dc26a42c74a0ebe2b3034f
Brandon Fosdick <bfoz@bfoz.net>
2012-04-01 22:11:53 -0700

    More testing of polygon construction

--- test/geometry/polygon.rb
+++ test/geometry/polygon.rb
@@ -2,7 +2,13 @@ require_relative '../helper'
 require_relative '../../lib/geometry/polygon'
 
 class PolygonTest < Test::Unit::TestCase
-    must "create a Polygon object" do
+    must "create a Polygon object with no arguments" do
+	polygon = Geometry::Polygon.new
+	assert_kind_of(Geometry::Polygon, polygon)
+	assert_equal(0, polygon.edges.size)
+	assert_equal(0, polygon.vertices.size)
+    end
+    must "create a Polygon object from array arguments" do
 	polygon = Geometry::Polygon.new([0,0], [1,0], [1,1], [0,1])
 	assert_kind_of(Geometry::Polygon, polygon)
 	assert_equal(4, polygon.edges.size)
