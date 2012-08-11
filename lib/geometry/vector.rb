5c31cd1224116f2544481fe2690ae7a9b2bee3dd
Brandon Fosdick <bfoz@bfoz.net>
2012-08-10 17:16:59 -0700

    Added Vector#cross

--- lib/geometry/vector.rb
+++ lib/geometry/vector.rb
@@ -11,4 +11,20 @@ class Vector
 	Vector[*(@elements.map {|e| -e })]
     end
 # @endgroup
+
+    # Cross-product of two {Vector}s
+    # @return [Vector]
+    def cross(other)
+	Vector.Raise ErrDimensionMismatch unless @elements.size == other.size
+	
+	case @elements.size
+	    when 0 then raise ArgumentError, "Can't multply zero-length Vectors"
+	    when 1 then @elements.first * other.first
+	    when 2 then @elements.first * other[1] - @elements.last * other.first
+	    when 3 then Vector[ @elements[1]*other[2] - @elements[2]*other[1],
+				@elements[2]*other[0] - @elements[0]*other[2],
+				@elements[0]*other[1] - @elements[1]*other[0]]
+	end
+    end
+    alias ** cross
 end
--- test/geometry/vector.rb
+++ test/geometry/vector.rb
@@ -13,5 +13,11 @@ describe Vector do
 	it "must have unary negation" do
 	    (-left).must_equal Vector[-1,-2]
 	end
+	
+	it "must cross product" do
+	    left.cross(right).must_equal -2
+	    Vector[1,2,3].cross(Vector[3,4,5]).must_equal Vector[-2, 4, -2]
+	    (Vector[1,2,3] ** Vector[3,4,5]).must_equal Vector[-2, 4, -2]
+	end
     end
 end
