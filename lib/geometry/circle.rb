d5c3d6d7a9de1cac730aa7b4e21e39a994588a53
Brandon Fosdick <bfoz@bfoz.net>
2012-08-26 15:19:28 -0700

    Added a diameter argument to Circle construction

--- lib/geometry/circle.rb
+++ lib/geometry/circle.rb
@@ -6,15 +6,49 @@ module Geometry
 Circles come in all shapes and sizes, but they're usually round.
 
 == Usage
-    circle = Geometry::Circle.new [1,1], 2
+    circle = Geometry::Circle.new [1,2], 3
+    circle = Geometry::Circle.new [1,2], :radius => 3
+    circle = Geometry::Circle.new [1,2], :diameter => 6
+    circle = Geometry::Circle.new :center => [1,2], :diameter => 6
 =end
 
     class Circle
-	# @return [Point]   The Circle's center point
+	# @return [Point]   The {Circle}'s center point
 	attr_reader :center
 
-	# @return [Number]  The Circle's radius
+	# @return [Number]  The {Circle}'s radius
 	attr_reader :radius
+
+	# @overload new(circle, radius)
+	#   Construct a {Circle} using a centerpoint and radius
+	#   @param [Point]	center  The center point of the {Circle}
+	#   @param [Number]	radius	The radius of the {Circle}
+	# @overload new(options)
+	#   Construct a circle using named center and radius parameters
+	#   @option options [Point]	:center
+	#   @option options [Number]	:radius
+	# @overload new(options)
+	#   Construct a circle using named center and diameter parameters
+	#   @option options [Point]	:center
+	#   @option options [Number]	:diameter
+	def self.new(*args, &block)
+	    options, args = args.partition {|a| a.is_a? Hash}
+	    options = options.reduce({}, :merge)
+	    center, radius = args[0..1]
+
+	    center = Point[center || options[:center]]
+	    raise ArgumentError, "Circle.new requires a center" unless center
+
+	    radius ||= options[:radius]
+
+	    if radius
+		self.allocate.tap {|circle| circle.send :initialize, center, radius, &block }
+	    elsif options.has_key?(:diameter)
+		CenterDiameterCircle.new center, options[:diameter], &block
+	    else
+		raise ArgumentError, "Circle.new requires a radius or a diameter"
+	    end
+	end
 
 	# Construct a new {Circle} from a centerpoint and radius
 	# @param    [Point]	center  The center point of the {Circle}
@@ -24,5 +58,34 @@ Circles come in all shapes and sizes, but they're usually round.
 	    @center = Point[center]
 	    @radius = radius
 	end
+	
+# @!group Accessors
+	# @!attribute [r] diameter
+	#   @return [Numeric] The diameter of the {Circle}
+	def diameter
+	    @radius*2
+	end
+# @!endgroup
+    end
+    
+    class CenterDiameterCircle < Circle
+	# @return [Number]  The {Circle}'s diameter
+	attr_reader :diameter
+
+	# Construct a new {Circle} from a centerpoint and a diameter
+	# @param    [Point]	center  The center point of the {Circle}
+	# @param    [Number]	diameter  The radius of the {Circle}
+	# @return   [Circle]	A new {Circle} object
+	def initialize(center, diameter)
+	    @center = Point[center]
+	    @diameter = diameter
+	end
+
+# @!group Accessors
+	# @return The {Circle}'s radius
+	def radius
+	    @diameter/2
+	end
+# @!endgroup
     end
 end
--- test/geometry/circle.rb
+++ test/geometry/circle.rb
@@ -1,23 +1,59 @@
 require 'minitest/autorun'
 require 'geometry/circle'
 
-def Circle(*args)
-    Geometry::Circle.new(*args)
-end
-
 describe Geometry::Circle do
-    it "must create a Circle object from a Point and a radius" do
-	circle = Circle [1,2], 3
-	assert_kind_of(Geometry::Circle, circle)
+    Circle = Geometry::Circle
+
+    describe "when constructed with center and radius arguments" do
+	let(:circle) { Circle.new [1,2], 3 }
+
+	it "must create a Circle" do
+	    circle.must_be_instance_of(Circle)
+	end
+	
+	it "must have a center point accessor" do
+	    circle.center.must_equal Point[1,2]
+	end
+	
+	it "must have a radius accessor" do
+	    circle.radius.must_equal 3
+	end
     end
 
-    it "must have a center point accessor" do
-	circle = Circle [1,2], 3
-	assert_equal(circle.center, [1,2])
+    describe "when constructed with named center and radius arguments" do
+	let(:circle) { Circle.new :center => [1,2], :radius => 3 }
+	
+	it "must create a Circle" do
+	    circle.must_be_instance_of(Circle)
+	end
+	
+	it "must have a center point accessor" do
+	    circle.center.must_equal Point[1,2]
+	end
+	
+	it "must have a radius accessor" do
+	    circle.radius.must_equal 3
+	end
     end
 
-    it "must have a radius accessor" do
-	circle = Circle [1,2], 3
-	assert_equal(3, circle.radius)
+    describe "when constructed with a center and diameter" do
+	let(:circle) { Circle.new [1,2], :diameter => 4 }
+
+	it "must be a CenterDiameterCircle" do
+	    circle.must_be_instance_of(Geometry::CenterDiameterCircle)
+	    circle.must_be_kind_of(Circle)
+	end
+
+	it "must have a center" do
+	    circle.center.must_equal Point[1,2]
+	end
+
+	it "must have a diameter" do
+	    circle.diameter.must_equal 4
+	end
+
+	it "must calculate the correct radius" do
+	    circle.radius.must_equal 2
+	end
     end
 end
