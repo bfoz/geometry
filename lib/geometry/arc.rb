6126dcaf5c6440323233cd27055f977b6adba9fd
Brandon Fosdick <bfoz@bfoz.net>
2012-10-21 20:03:00 -0700

    Added ThreePointArc

--- lib/geometry/arc.rb
+++ lib/geometry/arc.rb
@@ -1,5 +1,8 @@
 require_relative 'point'
 
+require_relative 'cluster_factory'
+require_relative 'point'
+
 module Geometry
 
 =begin rdoc
@@ -11,9 +14,30 @@ An {Arc} with its center at [1,1] and a radius of 2 that starts at the X-axis an
 =end
 
     class Arc
+	include ClusterFactory
+
 	attr_reader :center
 	attr_reader :radius
 	attr_reader :start_angle, :end_angle
+
+	# @overload new(center_point, start_point, end_point)
+	#  Create a new {Arc} given center, start and end {Point}s
+	# @param [Point]    center_point    The {Point} at the center
+	# @param [Point]    start_point	    The {Arc} starts at the start {Point}
+	# @param [Point]    end_point	    The {Point} where it all ends
+	# @overload initialize(center, radius, start_angle, end_angle)
+	#  Create a new {Arc} given a center, a radius and start and end angles
+	# @param [Point]    center	The {Point} at the center of it all
+	# @param [Numeric]  radius	Radius
+	# @param [Numeric]  start_angle	Starting angle
+	# @param [Numeric]  end_angle	Ending angle
+	def self.new(*args)
+	    if 4 == args.size
+		original_new(*args)
+	    elsif 3 == args.size
+		ThreePointArc.new(*args)
+	    end
+	end
 
 	# Construct a new {Arc}
 	# @overload initialize(center, radius, start_angle, end_angle)
@@ -28,16 +52,39 @@ An {Arc} with its center at [1,1] and a radius of 2 that starts at the X-axis an
 	    @end_angle = end_angle
 	end
 
-	# The starting point of the {Arc}
-	# @return [Point]
+	# @return [Point]   The starting point of the {Arc}
 	def first
 	    @center + @radius * Vector[Math.cos(@start_angle), Math.sin(@start_angle)]
 	end
 
-	# The end point of the {Arc}
-	# @return [Point]
+	# @return [Point]   The end point of the {Arc}
 	def last
 	    @center + @radius * Vector[Math.cos(@end_angle), Math.sin(@end_angle)]
 	end
+    end
+
+    class ThreePointArc < Arc
+	attr_reader :center
+	attr_reader :start, :end
+
+	# Contruct a new {Arc} given center, start and end {Point}s
+	#  Always assumes that the {Arc} is counter-clockwise. Reverse the order
+	#  of the start and end points to get an {Arc} that goes around the other way.
+	# @overload initialize(center_point, start_point, end_point)
+	# @param [Point]    center_point    The {Point} at the center
+	# @param [Point]    start_point	    The {Arc} starts at the start {Point}
+	# @param [Point]    end_point	    The {Point} where it all ends
+	def initialize(center_point, start_point, end_point)
+	    @center, @start, @end = [center_point, start_point, end_point].map {|p| Point[p]}
+	    raise ArgumentError unless [@center, @start, @end].all? {|p| p.is_a?(Point)}
+	end
+
+	# The starting point of the {Arc}
+	# @return [Point]
+	alias :first :start
+
+	# The end point of the {Arc}
+	# @return [Point]
+	alias :last :end
     end
 end
--- test/geometry/arc.rb
+++ test/geometry/arc.rb
@@ -10,4 +10,13 @@ describe Geometry::Arc do
 	arc.start_angle.must_equal 0
 	arc.end_angle.must_equal 90
     end
+
+    it "must create an Arc from center, start and end points" do
+	arc = Geometry::Arc.new [1,2], [3,4], [5,6]
+	arc.must_be_kind_of Geometry::Arc
+	arc.center.must_equal Point[1,2]
+	arc.first.must_equal Point[3,4]
+	arc.last.must_equal Point[5,6]
+    end
+
 end
