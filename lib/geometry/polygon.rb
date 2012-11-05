4ac08f520c19109db7930bff58c0c00e92defa98
Brandon Fosdick <bfoz@bfoz.net>
2012-11-04 16:30:17 -0800

    Added the Gift Wrap algorithm to Polygon for generating a convex hull
    This isn't the best algorithm, but it was quick and easy.

--- lib/geometry/polygon.rb
+++ lib/geometry/polygon.rb
@@ -31,7 +31,7 @@ An object representing a closed set of vertices and edges.
 	#   @return [Polygon]
 	def initialize(*args)
 	    args.map! {|a| (a.is_a?(Array) || a.is_a?(Vector)) ? Point[a] : a}
-	    raise(ArgumentError,'Unknown argument type') unless args.all? {|a| a.is_a?(Point) || a.is_a?(Edge) }
+	    args.each {|a| raise ArgumentError, "Unknown argument type #{a.class}" unless a.is_a?(Point) or a.is_a?(Edge) }
 
 	    @edges = [];
 	    @vertices = [];
@@ -69,7 +69,62 @@ An object representing a closed set of vertices and edges.
 	    @edges.push Edge.new(@edges.last.last, @edges.first.first) unless @edges.empty? || (@edges.last.last == @edges.first.first)
 	end
 
+	# Returns the convex hull of the {Polygon}
+	# @return [Polygon] A convex {Polygon}, or the original {Polygon} if it's already convex
+	def convex
+	    wrap
+	end
+
+	# Returns the convex hull using the {http://en.wikipedia.org/wiki/Gift_wrapping_algorithm Gift Wrapping algorithm}
+	#  This implementation was cobbled together from many sources, but mostly from this implementation of the {http://butunclebob.com/ArticleS.UncleBob.ConvexHullTiming Jarvis March}
+	# @return [Polygon]
+	def wrap
+	    # Start with a Point that's guaranteed to be on the hull
+	    leftmost_point = vertices.min_by {|v| v.x}
+	    current_point = vertices.select {|v| v.x == leftmost_point.x}.min_by {|v| v.y}
+
+	    current_angle = 0.0
+	    hull_points = [current_point]
+	    while true
+		min_angle = 4.0
+		min_point = nil
+		vertices.each do |v1|
+		    next if current_point.equal? v1
+		    angle = pseudo_angle_for_edge(current_point, v1)
+		    min_point, min_angle = v1, angle if (angle >= current_angle) && (angle <= min_angle)
+		end
+		current_angle = min_angle
+		current_point = min_point
+		break if current_point == hull_points.first
+		hull_points << min_point
+	    end
+	    Polygon.new *hull_points
+	end
+
 	private
+
+	# Return a number that increases with the slope of the {Edge}
+	# @return [Number]  A number in the range [0,4)
+	def pseudo_angle_for_edge(point0, point1)
+	    delta = Point[point1.x.to_f, point1.y.to_f] - Point[point0.x.to_f, point0.y.to_f]
+	    if delta.x >= 0
+		if delta.y >= 0
+		    quadrant_one_psuedo_angle(delta.x, delta.y)
+		else
+		    1 + quadrant_one_psuedo_angle(delta.y.abs, delta.x)
+		end
+	    else
+		if delta.y >= 0
+		    3 + quadrant_one_psuedo_angle(delta.y, delta.x.abs)
+		else
+		    2 + quadrant_one_psuedo_angle(delta.x.abs, delta.y.abs)
+		end
+	    end
+	end
+
+	def quadrant_one_psuedo_angle(dx, dy)
+	    dx / (dx + dy)
+	end
 
 	def push_edge(*e)
 	    @edges.push *e
--- test/geometry/polygon.rb
+++ test/geometry/polygon.rb
@@ -2,6 +2,8 @@ require 'minitest/autorun'
 require 'geometry/polygon'
 
 describe Geometry::Polygon do
+    Polygon = Geometry::Polygon
+
     it "must create a Polygon object with no arguments" do
 	polygon = Geometry::Polygon.new
 	assert_kind_of(Geometry::Polygon, polygon)
@@ -28,4 +30,37 @@ describe Geometry::Polygon do
 	assert_equal(4, polygon.vertices.size)
 	assert_equal(polygon.edges.first.first, polygon.edges.last.last)
     end
+
+    it "must gift wrap a square polygon" do
+	polygon = Polygon.new [0,0], [1,0], [1,1], [0,1]
+	convex_hull = polygon.wrap
+	convex_hull.must_be_kind_of Geometry::Polygon
+	convex_hull.edges.size.must_equal 4
+	convex_hull.vertices.must_equal [[0,0], [0,1], [1,1], [1,0]].map {|a| Point[*a]}
+    end
+
+    it "must gift wrap another square polygon" do
+	polygon = Polygon.new [0,1], [0,0], [1,0], [1,1]
+	convex_hull = polygon.wrap
+	convex_hull.must_be_kind_of Geometry::Polygon
+	convex_hull.edges.size.must_equal 4
+	convex_hull.vertices.must_equal [[0,0], [0,1], [1,1], [1,0]].map {|a| Point[*a]}
+    end
+
+    it "must gift wrap a concave polygon" do
+	polygon = Polygon.new [0,0], [1,-1], [2,0], [1,1], [2,2], [0,1]
+	convex_hull = polygon.wrap
+	convex_hull.must_be_kind_of Geometry::Polygon
+	convex_hull.edges.size.must_equal 5
+	convex_hull.vertices.must_equal [Point[0, 0], Point[0, 1], Point[2, 2], Point[2, 0], Point[1, -1]]
+    end
+
+    it "must gift wrap a polygon" do
+	polygon = Polygon.new [0,0], [1,-1], [2,0], [2,1], [0,1]
+	convex_hull = polygon.wrap
+	convex_hull.must_be_kind_of Geometry::Polygon
+	convex_hull.edges.size.must_equal 5
+	convex_hull.vertices.must_equal [[0,0], [0,1], [2,1], [2,0], [1,-1]].map {|a| Point[*a]}
+    end
+
 end
