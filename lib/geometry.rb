5290a7c169569cdecf63c7d8fc0053a9b2e6d0b3
Brandon Fosdick <bfoz@bfoz.net>
2013-05-15 22:10:25 -0700

    Removed the module-level line and point methods
    It was a silly idea

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -18,37 +18,4 @@ require_relative 'geometry/triangle'
 require_relative 'geometry/vector'
 
 module Geometry
-    # @overload Line(array0, array1)
-    #   @param [Array] array0 First endpoint
-    #   @param [Array] array1 Second endpoint
-    #   @return [TwoPointLine]
-    # @overload Line(point0, point1)
-    #   @param [Point] point0 First endpoint
-    #   @param [Point] point1 Second endpoint
-    #   @return [TwoPointLine]
-    # @overload Line(vector0, vector1)
-    #   @param [Vector] vector0 First endpoint
-    #   @param [Vector] vector1 Second endpoint
-    #   @return [TwoPointLine]
-    # @overload Line(y_intercept, slope)
-    #   @param [Numeric] y_intercept Y-intercept
-    #   @param [Numeric] slope Slope
-    #   @return [SlopeInterceptLine]
-    # @overload Line(point, slope)
-    #   @param [Point] point Starting point
-    #   @param [Numeric] slope Slope
-    #   @return [PointSlopeLine]
-    def self.line(*args)
-	Line[*args]
-    end
-
-    # @overload Point(x,y,z,...)
-    #   @return [Point]
-    # @overload Point(Point)
-    #   @return [Point]
-    # @overload Point(Vector)
-    #   @return [Point]
-    def self.point(*args)
-	Point[*args]
-    end
 end
--- test/geometry.rb
+++ test/geometry.rb
@@ -2,14 +2,4 @@ require 'minitest/autorun'
 require 'geometry'
 
 describe Geometry do
-    it "create a Point object" do
-	point = Geometry.point(2,1)
-	assert_kind_of(Geometry::Point, point)
-    end
-
-    it "create a Line object" do
-	line = Geometry.line([0,0], [10,10])
-	assert_kind_of(Geometry::Line, line)
-	assert_kind_of(Geometry::TwoPointLine, line)
-    end
 end
